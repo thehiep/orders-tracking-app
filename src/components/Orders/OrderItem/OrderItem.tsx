aeec291636c122ed35833a03e4217c2735439ef9
Charlie Bui <thehiep8x@gmail.com>
2021-05-26 05:38:44 +0700

    update card.

--- src/components/Orders/OrderItem/OrderItem.tsx
+++ src/components/Orders/OrderItem/OrderItem.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import classes from './OrderItem.module.css';
 import OrderItemDetail from './OrderItemDetail/OrderItemDetail';
 import Order from '../../../models/order'
+import Card from '../../UI/Card/Card';
 
 const OrderItem: React.FC<Order> = (props) => {
-    return <li>
-        <div>
-            <h1>ID:{props.id}</h1>
-            <p>Status: {props.status}</p>
-            <p>customer name: {props.customerName}</p>
-            <p>rider name: {props.riderName}</p>
-            <p>order address: {props.orderAddress}</p>
-            <p>merchant name: {props.merchantName}</p>
-            <p>merchant address: {props.merchantAddress}</p>
+    return <li className={classes['order-item']}>
+        <Card>
             <div>
-                <p>dishes:</p>
-                {props.dishes.map(item => <OrderItemDetail name={item.name} price={item.price} />)}
+                <h1>ID:{props.id}</h1>
+                <p>Status: {props.status}</p>
+                <p>customer name: {props.customerName}</p>
+                <p>rider name: {props.riderName}</p>
+                <p>order address: {props.orderAddress}</p>
+                <p>merchant name: {props.merchantName}</p>
+                <p>merchant address: {props.merchantAddress}</p>
+                <div>
+                    <p>dishes:</p>
+                    {props.dishes.map(item => <OrderItemDetail name={item.name} price={item.price} />)}
+                </div>
             </div>
-        </div>
+        </Card >
     </li>
 }
 
